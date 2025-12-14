d4e1ff616653a1639fee16b1bc62460478e17f7f
Dima Holubenko <amid310596@gmail.com>
2025-12-14 21:36:39 +0200

    Implement sorted_variables

--- app/main.py
+++ app/main.py
@@ -16,4 +16,40 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+lucky_number = 777                  # int (immutable)
+pi = 3.14                           # float (immutable)
+one_is_a_prime_number = False       # bool (immutable)
+name = "Richard"                    # str (immutable)
+
+my_favourite_films = [              # list (mutable)
+    "The Shawshank Redemption",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
+]
+
+profile_info = ("michel", "michel@gmail.com", "12345678")  # tuple (immutable)
+
+marks = {                           # dict (mutable)
+    "John": 4,
+    "Sergio": 3,
+}
+
+collection_of_coins = {1, 2, 25}    # set (mutable)
+
+sorted_variables = {
+    "mutable": [
+        my_favourite_films,
+        marks,
+        collection_of_coins,
+    ],
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+        profile_info,
+    ],
+}
+
